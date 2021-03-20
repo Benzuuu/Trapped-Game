a8e0e89c3ee5863440f3c6287b0600bf1e9dc3d9
unknown <hitman_reborn1356@yahoo.com>
2021-03-20 16:15:19 +0800

    Update

--- Trapped/Assets/Scripts/PlayerMovement.cs
+++ Trapped/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //speed of the character
+    public float speed = 10.0f;
+
+    //variable for the Input
+    private float HorizontalInput;
+    private float VerticalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //GetAxis
+        HorizontalInput = Input.GetAxis("Horizontal");
+        VerticalInput = Input.GetAxis("Vertical");
+
+        //Move Vertically
+        transform.Translate(Vector2.up * Time.deltaTime * speed * VerticalInput);
+        //Move Horizontally
+        transform.Translate(Vector2.right * Time.deltaTime * speed * HorizontalInput);
+
         
     }
 }
