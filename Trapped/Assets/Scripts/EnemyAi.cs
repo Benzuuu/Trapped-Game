f316f9cd19c4b9bda79fba6ca37fa150febc1fb4
slashkoopa <49096720+slashkoopa@users.noreply.github.com>
2021-04-17 18:35:18 +0800

    Enemy chase (no pathfinding)
    aaaaaa

---
+++ Trapped/Assets/Scripts/EnemyAi.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAi : MonoBehaviour
+{
+    public int health;
+    public string enemyName;
+    public int baseAttack;
+    public float moveSpeed;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
